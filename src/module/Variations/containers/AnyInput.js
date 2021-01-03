b4993c3af41462164615b13d089f7addd101ded1
Ahmed Mezar <a.mezar@lodexsolutions.local>
2021-01-03 10:59:57 +0200

    add total characters

--- src/module/Variations/containers/AnyInput.js
+++ src/module/Variations/containers/AnyInput.js
@@ -3,198 +3,217 @@ import { FormGroup, Input } from "reactstrap";
 import data from "../../../data/data.json";
 import { connect } from "react-redux";
 import displayResult from "../../../appRedux/actions/result";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Alert, Button } from 'reactstrap';
-
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Alert, Button } from "reactstrap";
 
 class AnyInput extends Component {
   state = {
-    userInput: '',
+    userInput: "",
     userInputArr: [],
     selectedWord: "",
     copied: false,
     isTooltipOpen: false,
     displayAlert: false,
     booleanQuery: false,
-    variationsDisplayed: []
-  }
+    variationsDisplayed: [],
+  };
 
   getselectword = () => {
     let curPos = document.getElementById("text1").selectionStart;
-    let totalLenth = 0
+    let totalLenth = 0;
     let selectedWord = null;
     let arrayIndex = null;
-    let secondWord = null
-    let thirdWord = null
-    this.state.userInputArr && this.state.userInputArr.map((item, index) => {
-      totalLenth += item.length;
+    let secondWord = null;
+    let thirdWord = null;
+    this.state.userInputArr &&
+      this.state.userInputArr.map((item, index) => {
+        totalLenth += item.length;
 
-      if (index === 0 && totalLenth >= curPos) {
-        selectedWord = item;
-        console.log(curPos, totalLenth, selectedWord, "1")
-      }
-      else if (index > 0 && !selectedWord && totalLenth >= curPos - index) {
-        selectedWord = item;
-        console.log(curPos, totalLenth, selectedWord, "2")
-      }
-      else if (!selectedWord && totalLenth >= curPos - index) {
-        selectedWord = item
-        console.log(curPos, totalLenth, selectedWord, "3")
-      }
-
-    })
-    arrayIndex = this.state.userInputArr.findIndex((i) => i === selectedWord)
+        if (index === 0 && totalLenth >= curPos) {
+          selectedWord = item;
+          console.log(curPos, totalLenth, selectedWord, "1");
+        } else if (index > 0 && !selectedWord && totalLenth >= curPos - index) {
+          selectedWord = item;
+          console.log(curPos, totalLenth, selectedWord, "2");
+        } else if (!selectedWord && totalLenth >= curPos - index) {
+          selectedWord = item;
+          console.log(curPos, totalLenth, selectedWord, "3");
+        }
+      });
+    arrayIndex = this.state.userInputArr.findIndex((i) => i === selectedWord);
     if (arrayIndex !== -1) {
       if (arrayIndex === 0) {
         secondWord = this.state.userInputArr[arrayIndex + 1];
         thirdWord = this.state.userInputArr[arrayIndex + 2];
-      }
-      else if (arrayIndex === this.state.userInputArr.length - 1) {
+      } else if (arrayIndex === this.state.userInputArr.length - 1) {
         secondWord = this.state.userInputArr[arrayIndex - 1];
         thirdWord = this.state.userInputArr[arrayIndex - 2];
-      }
-      else {
+      } else {
         secondWord = this.state.userInputArr[arrayIndex - 1];
         thirdWord = this.state.userInputArr[arrayIndex + 1];
       }
-
     }
 
     this.setState({ selectedWord });
-    console.log(selectedWord, secondWord, thirdWord)
-    this.getResulte(selectedWord, secondWord, thirdWord, arrayIndex)
-
-  }
+    console.log(selectedWord, secondWord, thirdWord);
+    this.getResulte(selectedWord, secondWord, thirdWord, arrayIndex);
+  };
 
   getResulte = async (selected, secondSelected, thirdSelected, wordIndex) => {
     const { result, displayResult } = this.props;
     let variations = result;
-    let WordsKeys = []
+    let WordsKeys = [];
     let isfound = await this.check(selected);
     if (isfound) {
       data.map((Item) => {
         if (Item.word === selected) {
-          WordsKeys.unshift(Item)
-          console.log(WordsKeys, "1")
-        }
-        else if (Item.word === secondSelected) {
+          WordsKeys.unshift(Item);
+          console.log(WordsKeys, "1");
+        } else if (Item.word === secondSelected) {
           if (WordsKeys.length > 2) {
-            WordsKeys.splice(1, 0, Item)
+            WordsKeys.splice(1, 0, Item);
           } else if (wordIndex === this.state.userInputArr.length - 1) {
-            WordsKeys.unshift(Item)
+            WordsKeys.unshift(Item);
           } else {
-            WordsKeys.push(Item)
+            WordsKeys.push(Item);
           }
-          console.log(WordsKeys, "2")
-        }
-        else if (Item.word === thirdSelected) {
-          WordsKeys.push(Item)
-          console.log(WordsKeys, "3")
+          console.log(WordsKeys, "2");
+        } else if (Item.word === thirdSelected) {
+          WordsKeys.push(Item);
+          console.log(WordsKeys, "3");
         }
       });
 
       variations = WordsKeys;
 
-      console.log(variations)
-      displayResult(variations)
-
+      console.log(variations);
+      displayResult(variations);
     }
-
-  }
+  };
   covert = (e) => {
     this.setState({ userInput: e.target.value }, () => {
-      this.initializeInput()
-    })
-  }
+      this.initializeInput();
+    });
+  };
 
   initializeInput = () => {
     // let userInputSpaces = this.state.userInput.replace(/\s/g, '')
-    let userInputArr = this.state.userInput.split(",")
-    let final = userInputArr.map(elment => {
+    let userInputArr = this.state.userInput.split(",");
+    let final = userInputArr.map((elment) => {
       return elment.trim();
-    })
-    console.log(final)
+    });
+    console.log(final);
 
-    this.setState({ userInputArr: final })
-  }
-
+    this.setState({ userInputArr });
+  };
 
   async check(selectedWord) {
     const { result } = this.props;
     if (result && result.length > 0 && result[0].word === selectedWord) {
-      return false
-    }
-    else {
-      return true
+      return false;
+    } else {
+      return true;
     }
   }
 
   onAlerDismiss = () => {
-    this.setState({ displayAlert: false })
-  }
+    this.setState({ displayAlert: false });
+  };
   componentDidUpdate(prevProps) {
     if (prevProps.result !== this.props.result) {
-      this.setState({ variationsDisplayed: this.props.result })
+      this.setState({ variationsDisplayed: this.props.result });
     }
   }
 
-
   render() {
-
     const { displayAlert, variationsDisplayed } = this.state;
-    const { booleanQuery, result } = this.props
-    console.log(this.state.variationsDisplayed)
+    const { booleanQuery, result } = this.props;
+    console.log(this.state.variationsDisplayed);
     return (
       <div className="input-container">
         <span className="boolean">Boolean Query</span>
-        
+
         <FormGroup>
-          <Input type="textarea" name="text" id="text1" rows="6" value={this.state.userInput} onChange={(e) => this.covert(e)} onClick={() => this.getselectword()} />
+          <Input
+            type="textarea"
+            name="text"
+            id="text1"
+            rows="6"
+            value={this.state.userInput}
+            onChange={(e) => this.covert(e)}
+            onClick={() => this.getselectword()}
+            maxLength={450}
+          />
         </FormGroup>
+        <div className="total-Char">
+          Total Characters {this.state.userInput.length} / 450
+        </div>
         <div className="results">
-          <Button onClick={() => this.setState({ variationsDisplayed: null, userInput: "" })}>Rest</Button>
+          <Button
+            onClick={() =>
+              this.setState({ variationsDisplayed: null, userInput: "" })
+            }
+          >
+            Rest
+          </Button>
           <p>
             Keyword variation
-        <span>
+            <span>
               <i className="fa fa-question-circle"></i>
             </span>
           </p>
-          {
-            displayAlert && <Alert color="primary" isOpen={displayAlert} toggle={this.onAlerDismiss}>
+          {displayAlert && (
+            <Alert
+              color="primary"
+              isOpen={displayAlert}
+              toggle={this.onAlerDismiss}
+            >
               text copied to the clipboard
-        </Alert  >
-          }
-          {
-            variationsDisplayed && variationsDisplayed.map((Item, index) => {
+            </Alert>
+          )}
+          {variationsDisplayed &&
+            variationsDisplayed.map((Item, index) => {
               return (
                 <div className="keyword-variations" key={index}>
-                  <span className="main-word">{Item.word}<i className="fa fa-arrow-circle-right arrowIcon"></i></span>
+                  <span className="main-word">
+                    {Item.word}
+                    <i className="fa fa-arrow-circle-right arrowIcon"></i>
+                  </span>
                   <div className="variations-container">
-                    {
-                      Item.variations.map((variation, key) => {
-                        return (
-                          <CopyToClipboard text={variation} onCopy={() => this.setState({ copied: true, displayAlert: true })} key={key} ><span className="variation">{variation}</span></CopyToClipboard>
-                        )
-
-                      })
-                    }
-                    <CopyToClipboard text={booleanQuery ? Item.variations : Item.variations.join(" OR ")} onCopy={() => this.setState({ copied: true, displayAlert: true })}>
+                    {Item.variations.map((variation, key) => {
+                      return (
+                        <CopyToClipboard
+                          text={variation}
+                          onCopy={() =>
+                            this.setState({ copied: true, displayAlert: true })
+                          }
+                          key={key}
+                        >
+                          <span className="variation">{variation}</span>
+                        </CopyToClipboard>
+                      );
+                    })}
+                    <CopyToClipboard
+                      text={
+                        booleanQuery
+                          ? Item.variations
+                          : Item.variations.join(" OR ")
+                      }
+                      onCopy={() =>
+                        this.setState({ copied: true, displayAlert: true })
+                      }
+                    >
                       <span className="copy-all">Copy All</span>
                     </CopyToClipboard>
                   </div>
-
                 </div>
-              )
-            })
-
-          }
+              );
+            })}
         </div>
       </div>
     );
   }
 }
-
-
 
 const mapStateToProps = (state) => {
   return {
@@ -205,7 +224,6 @@ const mapStateToProps = (state) => {
 const mapDispachToProps = (dispach) => {
   return {
     displayResult: (result) => dispach(displayResult(result)),
-
   };
 };
 
